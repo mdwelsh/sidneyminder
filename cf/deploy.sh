18e49db5ce8b1c37c30772b80db3c0eebc7c0c11
Matt Welsh <mdw@mdw.la>
2021-01-24 19:18:55 -0800

    Speech works.

--- cf/deploy.sh
+++ cf/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-gcloud functions deploy fetch_events \
+#gcloud functions deploy fetch_events \
+#  --runtime python37 --trigger-http --allow-unauthenticated
+
+gcloud functions deploy speak \
   --runtime python37 --trigger-http --allow-unauthenticated
--- cf/main.py
+++ cf/main.py
@@ -2,16 +2,17 @@
 
 import datetime
 import sys
+from typing import Any, Dict
 
 import flask
 from googleapiclient.discovery import build
+from google.cloud import texttospeech
 
 CALENDAR = "c_0o20o0ld5alqctcj6f2981kh34@group.calendar.google.com"
 
 
-def fetch_events(request: flask.Request) -> str:
-    print("MDW: fetch_events() called")
-    print(f"MDW: request is: {request}")
+def fetch_events(request: flask.Request) -> Dict[str, Any]:
+    print("MDW: fetch_events() called, request: {request}")
     request_json = request.get_json(silent=True)
     request_args = request.args
 
@@ -19,7 +20,6 @@ def fetch_events(request: flask.Request) -> str:
         maxResults = int(request_json["maxResults"])
     else:
         maxResults = 10
-
     print(f"MDW: maxResults is: {maxResults}")
 
     service = build("calendar", "v3")
@@ -43,7 +43,39 @@ def fetch_events(request: flask.Request) -> str:
         return {"result": []}
     return {"result": events}
 
-#    for event in events:
-#        start = event["start"].get("dateTime", event["start"].get("date"))
-#        print(start, event["summary"])
 
+def speak(request: flask.Request) -> str:
+    print(f"MDW: speak() called, request: {request}")
+    request_json = request.get_json(silent=True)
+
+    text = "You are a derp."
+    languageCode = "en-GB"
+    voice = "en-GB-Wavenet-F"
+
+    if request_json:
+        if "text" in request_json:
+            text = request_json["text"]
+        if "languageCode" in request_json:
+            languageCode = request_json["languageCode"]
+        if "voice" in request_json:
+            voice = request_json["voice"]
+    else:
+        text = "You are a derp."
+    print(f"MDW: text is: {text}")
+    print(f"MDW: languageCode is: {languageCode}")
+    print(f"MDW: voice is: {voice}")
+
+    client = texttospeech.TextToSpeechClient()
+
+    input = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=languageCode,
+        name=voice
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.LINEAR16
+    )
+    response = client.synthesize_speech(
+        input=input, voice=voice, audio_config=audio_config
+    )
+    return (response.audio_content, 200, {"Content-Type": "application/octet-stream"})
