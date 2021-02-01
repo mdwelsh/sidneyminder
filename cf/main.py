8686f41df3afb7554083435d554bf4a0407e6bbd
Matt Welsh <mdw@mdw.la>
2021-01-31 21:10:35 -0800

    Fix up date processing

--- cf/main.py
+++ cf/main.py
@@ -4,9 +4,11 @@ import datetime
 import sys
 from typing import Any, Dict
 
+import arrow
 import flask
 from googleapiclient.discovery import build
 from google.cloud import texttospeech
+import humanize
 
 CALENDAR = "c_0o20o0ld5alqctcj6f2981kh34@group.calendar.google.com"
 
@@ -24,12 +26,19 @@ def fetch_events(request: flask.Request) -> Dict[str, Any]:
 
     service = build("calendar", "v3")
 
-    now = datetime.datetime.utcnow().isoformat() + "Z"
+    now = datetime.datetime.utcnow()
+    now_iso = now.isoformat() + "Z"
+
+    # For human formatting
+    anow = arrow.utcnow().to("US/Pacific")
+    datestring = anow.strftime("%A, %B %-d")
+    timestring = anow.strftime("%-I:%M %p")
+
     events_result = (
         service.events()
         .list(
             calendarId=CALENDAR,
-            timeMin=now,
+            timeMin=now_iso,
             maxResults=maxResults,
             singleEvents=True,
             orderBy="startTime",
@@ -37,7 +46,29 @@ def fetch_events(request: flask.Request) -> Dict[str, Any]:
         .execute()
     )
     events = events_result.get("items", [])
-    print(f"MDW: Got events: {events}")
+
+    # Parse out timestamps since CircuitPython doesn't have datetime.
+    for event in events:
+        event["now"] = {
+            "date": datestring,
+            "time": timestring,
+            "dateTime": now_iso,
+            "timestamp": int(now.timestamp()),
+        }
+        st = event["start"]["dateTime"]
+        et = event["end"]["dateTime"]
+        try:
+            stp = datetime.datetime.strptime(st, "%Y-%m-%dT%H:%M:%S%z")
+            etp = datetime.datetime.strptime(et, "%Y-%m-%dT%H:%M:%S%z")
+            event["start"]["timestamp"] = int(stp.timestamp())
+            event["start"]["when"] = stp.strftime("%-I:%M %p")
+            until = humanize.naturaldelta(arrow.get(stp) - anow)
+            event["start"]["until"] = until
+            event["end"]["timestamp"] = int(etp.timestamp())
+        except Exception as e:
+            warning = f"Warning: {e}"
+            print(warning)
+            event["start"]["warning"] = warning
 
     if not events:
         return {"result": []}
@@ -69,8 +100,7 @@ def speak(request: flask.Request) -> str:
 
     input = texttospeech.SynthesisInput(text=text)
     voice = texttospeech.VoiceSelectionParams(
-        language_code=languageCode,
-        name=voice
+        language_code=languageCode, name=voice
     )
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.LINEAR16
@@ -78,4 +108,8 @@ def speak(request: flask.Request) -> str:
     response = client.synthesize_speech(
         input=input, voice=voice, audio_config=audio_config
     )
-    return (response.audio_content, 200, {"Content-Type": "application/octet-stream"})
+    return (
+        response.audio_content,
+        200,
+        {"Content-Type": "application/octet-stream"},
+    )
